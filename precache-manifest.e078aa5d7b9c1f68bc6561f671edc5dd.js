b9e6e41e319905262087f55160ca10c143d71833
root <saood011@yahoo.com>
2020-02-11 11:03:09 +0100

    Updates

---
+++ precache-manifest.e078aa5d7b9c1f68bc6561f671edc5dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0406a70830db7c6f62db91690ec2a1a4",
+    "url": "/techstore/index.html"
+  },
+  {
+    "revision": "014a96f89eb95ae69018",
+    "url": "/techstore/static/css/main.ff200364.chunk.css"
+  },
+  {
+    "revision": "e214bc65c83e9eda1ae9",
+    "url": "/techstore/static/js/2.7dadaa61.chunk.js"
+  },
+  {
+    "revision": "014a96f89eb95ae69018",
+    "url": "/techstore/static/js/main.6f5c55dd.chunk.js"
+  },
+  {
+    "revision": "b5f68306f4c4cf555544",
+    "url": "/techstore/static/js/runtime-main.009ce497.js"
+  },
+  {
+    "revision": "635065a940cf2e414bb0a42a0db861ff",
+    "url": "/techstore/static/media/bg-image.635065a9.jpeg"
+  }
+]);
